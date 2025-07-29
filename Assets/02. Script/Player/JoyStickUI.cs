01604aa440e28519de12c4834024146c68b7327d
sang1kev <sang1kev@naver.com>
2025-07-29 16:10:31 +0900

    [Player] script, anim update

--- Assets/02. Script/Player/JoyStickUI.cs
+++ Assets/02. Script/Player/JoyStickUI.cs
@@ -1,32 +1,33 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEditor.Experimental.GraphView;
+using Unity.VisualScripting;
 
 public class JoyStickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    private PlayerFSM pCtrl;
+    private PlayerCtrl playerCtrl;
 
     [SerializeField] private GameObject backgroundUI;
     [SerializeField] private GameObject handlerUI;
 
-    private Vector3 startPos, currPos;
+    [SerializeField] private float maxDist = 95f;
+
+    private Vector3 startPos, currPos, playerDir;
 
     void Start()
     {
-        pCtrl = FindFirstObjectByType<PlayerFSM>();
+        playerCtrl = FindFirstObjectByType<PlayerCtrl>();
         backgroundUI.SetActive(false);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         currPos = eventData.position;
-        Vector3 dragDir = currPos - startPos;
+        Vector3 dragDir = (currPos - startPos);
+    
+        float distance = Mathf.Min(dragDir.magnitude, maxDist);
 
-        float maxDist = Mathf.Min(dragDir.magnitude, 95f);
-
-        handlerUI.transform.position = startPos + dragDir.normalized * maxDist;
-
-        pCtrl.InputJoyStick(dragDir.normalized.x, dragDir.normalized.y);
+        handlerUI.transform.position = startPos + dragDir.normalized * distance;
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -34,12 +35,23 @@ public class JoyStickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
         backgroundUI.SetActive(true);
         backgroundUI.transform.position = eventData.position;
         startPos = eventData.position;
+        playerDir = Vector3.zero;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        Vector3 finalPos = eventData.position;
+        Vector3 finalDir = (finalPos - startPos);
+
+        float distance = Mathf.Min(finalDir.magnitude, maxDist);
+        float movePower = distance / maxDist;
+
+        int invDir = -1;
+        Vector3 playerDir = invDir * movePower * finalDir.normalized;
+
+        playerCtrl.InputJoyStick(playerDir.x, playerDir.y);
+
         handlerUI.transform.localPosition = Vector3.zero;
-        pCtrl.InputJoyStick(0, 0);
         backgroundUI.SetActive(false);
     }
 }
