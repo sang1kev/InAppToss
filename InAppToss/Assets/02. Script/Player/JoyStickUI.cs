cdcf9f8290f03a671ec818ebe45760d6c07ad915
Song Hogeun <102279006+song-Hogeun@users.noreply.github.com>
2025-07-30 16:16:26 +0900

    [InAppToss] Block Animation
    1. 블록 애니메이션 ( position, rotation, 불투명도 등 ) 구현
    2. 플레이어 태그 'Player' 로 추가 ( 충돌 감지 위함 )
    3. Block 스크립트 수정 ( 충돌 감지, 애니메이션 실행 )

---
+++ InAppToss/Assets/02. Script/Player/JoyStickUI.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEditor.Experimental.GraphView;
+using Unity.VisualScripting;
+
+public class JoyStickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    private PlayerCtrl playerCtrl;
+
+    [SerializeField] private GameObject backgroundUI;
+    [SerializeField] private GameObject handlerUI;
+
+    [SerializeField] private float maxDist = 95f;
+
+    private Vector3 startPos, currPos, playerDir;
+
+    void Start()
+    {
+        playerCtrl = FindFirstObjectByType<PlayerCtrl>();
+        backgroundUI.SetActive(false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currPos = eventData.position;
+        Vector3 dragDir = (currPos - startPos);
+    
+        float distance = Mathf.Min(dragDir.magnitude, maxDist);
+
+        handlerUI.transform.position = startPos + dragDir.normalized * distance;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        backgroundUI.SetActive(true);
+        backgroundUI.transform.position = eventData.position;
+        startPos = eventData.position;
+        playerDir = Vector3.zero;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Vector3 finalPos = eventData.position;
+        Vector3 finalDir = (finalPos - startPos);
+
+        float distance = Mathf.Min(finalDir.magnitude, maxDist);
+        float movePower = distance / maxDist;
+
+        int invDir = -1;
+        Vector3 playerDir = invDir * movePower * finalDir.normalized;
+
+        playerCtrl.InputJoyStick(playerDir.x, playerDir.y);
+
+        handlerUI.transform.localPosition = Vector3.zero;
+        backgroundUI.SetActive(false);
+    }
+}
