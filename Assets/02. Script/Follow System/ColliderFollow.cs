9d92cdfb9238d99036b3fc4361c0ea2fbe5178bb
sang1kev <sang1kev@naver.com>
2025-08-01 14:11:20 +0900

    [PlayTest] player follow script added

---
+++ Assets/02. Script/Follow System/ColliderFollow.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ColliderFollow : MonoBehaviour
+{
+    private Transform target;
+
+    [SerializeField] private float offset = -5;
+    [SerializeField] private float minBoundary = 0f;
+    [SerializeField] private float damp = 10;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        float targetPosY = target.position.y + offset;
+
+        if (targetPosY > minBoundary)
+        {
+            minBoundary = targetPosY;
+        }
+
+        Vector3 pos = transform.position;
+        pos.y = minBoundary + offset;
+        transform.position = pos;
+    }
+}
