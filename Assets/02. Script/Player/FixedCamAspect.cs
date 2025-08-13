81b3f753e6dbc5890eba961ed7ff729a0f74fa7e
sang1kev <sang1kev@naver.com>
2025-08-13 17:29:55 +0900

    [WebGL] Sceen test

--- Assets/02. Script/Player/FixedCamAspect.cs
+++ Assets/02. Script/Player/FixedCamAspect.cs
@@ -3,30 +3,34 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class FixedAspect : MonoBehaviour
 {
-    public float targetAspect = 720f / 1280f;
+    private float targetAspectWidth = 720f;
+    private float targetAspectHeight = 1280f;
 
     void Start()
     {
         Camera cam = GetComponent<Camera>();
+        float targetAspect = targetAspectWidth / targetAspectHeight;
         float windowAspect = (float)Screen.width / Screen.height;
         float scaleHeight = windowAspect / targetAspect;
 
-        if (scaleHeight < 1f)
+        if (scaleHeight < 1.0f)
         {
+            // Letterbox (위아래 검은 바)
             Rect rect = cam.rect;
-            rect.width = 1f;
+            rect.width = 1.0f;
             rect.height = scaleHeight;
             rect.x = 0;
-            rect.y = (1f - scaleHeight) / 2f;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
             cam.rect = rect;
         }
         else
         {
-            float scaleWidth = 1f / scaleHeight;
+            // Pillarbox (좌우 검은 바)
+            float scaleWidth = 1.0f / scaleHeight;
             Rect rect = cam.rect;
             rect.width = scaleWidth;
-            rect.height = 1f;
-            rect.x = (1f - scaleWidth) / 2f;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
             rect.y = 0;
             cam.rect = rect;
         }
