c6a9125a6d187dc2d06fbc19a8999308d76a9f45
sang1kev <sang1kev@naver.com>
2025-08-26 16:17:36 +0900

    [Cam fix]

--- Assets/02. Script/Player/FixedCamAspect.cs
+++ Assets/02. Script/Player/FixedCamAspect.cs
@@ -1,38 +1,38 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class FixedAspect : MonoBehaviour
+public class CameraAspect : MonoBehaviour
 {
-    private float targetAspectWidth = 720f;
-    private float targetAspectHeight = 1280f;
+    private Camera cam;
 
-    void Start()
+    private float baseWidth = 720f;
+    private float baseHeight = 1280f;
+
+    void Awake()
     {
-        Camera cam = GetComponent<Camera>();
-        float targetAspect = targetAspectWidth / targetAspectHeight;
-        float windowAspect = (float)Screen.width / Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
+        cam = GetComponent<Camera>();
+        UpdateView();
+    }
 
-        if (scaleHeight < 1.0f)
+    void Update()
+    {
+        UpdateView();
+    }
+
+    void UpdateView()
+    {
+        float targetAspect = baseWidth / baseHeight;
+        float windowAspect = (float)Screen.width / Screen.height;
+
+        float baseSize = baseHeight / 200f;
+
+        if (windowAspect > targetAspect)
         {
-            // Letterbox (위아래 검은 바)
-            Rect rect = cam.rect;
-            rect.width = 1.0f;
-            rect.height = scaleHeight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleHeight) / 2.0f;
-            cam.rect = rect;
+            cam.orthographicSize = baseSize;
         }
         else
         {
-            // Pillarbox (좌우 검은 바)
-            float scaleWidth = 1.0f / scaleHeight;
-            Rect rect = cam.rect;
-            rect.width = scaleWidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scaleWidth) / 2.0f;
-            rect.y = 0;
-            cam.rect = rect;
+            cam.orthographicSize = baseSize * (targetAspect / windowAspect);
         }
     }
 }
