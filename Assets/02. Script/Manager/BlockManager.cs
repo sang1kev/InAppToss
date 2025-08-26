1cdbae1223f2558572c09134fe82c8f67a11bc9a
sang1kev <sang1kev@naver.com>
2025-08-26 17:19:10 +0900

    update

--- Assets/02. Script/Manager/BlockManager.cs
+++ Assets/02. Script/Manager/BlockManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using TMPro;
 using UnityEngine;
 
 public class BlockManager : MonoBehaviour
@@ -102,9 +101,7 @@ public class BlockManager : MonoBehaviour
     {
         float lastX = float.MinValue;
         const float minDist = 1.5f;
-        float maxJumpY = playerCtrl.DidPlayerExit
-            ? playerCtrl.transform.position.y + Time.deltaTime
-            : playerCtrl.transform.position.y;
+        float maxJumpY = playerCtrl.DidPlayerExit ? playerCtrl.transform.position.y + Time.deltaTime : playerCtrl.transform.position.y;
 
         while (true)
         {
@@ -113,7 +110,7 @@ public class BlockManager : MonoBehaviour
                 yield return null;
                 continue;
             }
-
+            
             int x;
             int tries = 10;
             do
@@ -122,9 +119,9 @@ public class BlockManager : MonoBehaviour
             } while (Mathf.Abs(x - lastX) < minDist && --tries > 0);
 
             lastX = x;
-
+            
             float currY = playerCtrl.transform.position.y;
-
+            
             if (playerCtrl.ISDashAvail)
             {
                 if (currY > maxJumpY)
@@ -134,32 +131,10 @@ public class BlockManager : MonoBehaviour
             {
                 maxJumpY = currY;
             }
-
+            
             float blockY = playerCtrl.ISDashAvail ? maxJumpY + 13f : currY + 10f;
-
-            int ranNum = Random.Range(0, 101);
-            int ranItem = Random.Range(0, 2);
-
+            
             Block block = ObjectPool.GetObject();
-            
-            block.GetComponentInChildren<TextMeshProUGUI>().text = "";
-            
-            if (ranNum == 10 || ranNum == 20)
-            {
-                switch (ranItem)
-                {
-                    case 0:
-                        block.GetComponentInChildren<TextMeshProUGUI>().text = "J";
-                        break;
-                    case 1:
-                        block.GetComponentInChildren<TextMeshProUGUI>().text = "L";
-                        break;
-                    case 2:
-                        block.GetComponentInChildren<TextMeshProUGUI>().text = "G";
-                        break;
-                }
-            }
-            
             block.transform.position = new Vector3(x, blockY, 0f);
 
             if (uiManager.IsGameStarted) spawnTime = Random.Range(0.2f, 0.3f);
